47ed45770bf2f3587a15b7879e58678c41006087
BurhanSami <95189551+BurhanO2@users.noreply.github.com>
2026-08-12 14:50:36 +0500

    Solve expected-value-discrete

---
+++ expected-value-discrete/expected-value-discrete.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def expected_value_discrete(x, p):
+    """
+    Returns: float expected value
+    """
+    # Write code here
+    x = np.asarray(x, dtype=float)
+    p = np.asarray(p, dtype=float)
+
+    if np.sum(p) != 1.0:
+        raise ValueError
+    
+    print(x * p)
+    return np.dot(x, p)
